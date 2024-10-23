90684190d88e62072c215f19f1f16953cb910ad9
Zack <zackarno@gmail.com>
2024-10-23 13:22:47 -0400

    add docstrings

--- exploration/01_baseline_calc_10yr_doy.py
+++ exploration/01_baseline_calc_10yr_doy.py
@@ -1,10 +1,24 @@
+"""
+This script calculates the DOY baseline for FloodScan raster data at pixel
+level. It is currently in `exploratory` repo as it just uses GDRIVE `.nc` file
+to make local development more effecient (vs loading historical data from blob).
+
+In the official pipeline we can link it to the historical tif record on the blob.
+
+Steps: 
+
+1. Smooth historical data w/ 10 day rolling mean (centered)
+2. filter data to last 10 years of data (exlcusive of current year)
+3. Calculate the mean SFED value per day of year from smoothed raster stack.
+"""
+
 import pandas as pd
 import numpy as np
 import xarray as xr
 import datetime
 import matplotlib.pyplot as plt
 
-from src.datasources import floodscans
+from src.datasources import floodscan
 
 # Get today's date and save as run_date
 run_date = datetime.datetime.today()
@@ -26,7 +40,6 @@ da_fs_smooth_filt = da_fs_smooth.sel(
     time=da_fs_smooth["time.year"].isin(last_10_years)
 )
 
-
 # calculate mean per DOY
 da_doy_mean = da_fs_smooth_filt.groupby("time.dayofyear").mean("time")
 
@@ -37,7 +50,7 @@ days_to_plot = [1, 10, 200]
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
 for ax, day in zip(axes, days_to_plot):
-    doy_mean.sel(dayofyear=day).plot(ax=ax)
+    da_doy_mean.sel(dayofyear=day).plot(ax=ax)
     ax.set_title(f"Day {day}")
 
 plt.tight_layout()
