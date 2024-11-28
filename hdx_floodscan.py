2a66030ef5f467cb3db489e4f5ecc48b9d1fa919
Isa Tot <isabelle.tot@un.org>
2024-11-28 17:11:51 +0000

    Trying to fix databricks path issue [3]

--- hdx_floodscan.py
+++ hdx_floodscan.py
@@ -182,10 +182,7 @@ class HDXFloodscan:
 
     def _generate_zipped_file(self, last90_days_geotiffs, ds_historical_baseline):
 
-        base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-        geotiffs_dir = Path("geotiffs")
-        new_dir = base_dir / geotiffs_dir
-        new_dir.mkdir(exist_ok=True)
+        os.mkdir("geotiffs")
 
         out_files = []
         for ds_current_sfed in last90_days_geotiffs:
