1af054417e1b6ab872d0c777aff7a176801cd924
Isa Tot <isabelle.tot@un.org>
2024-11-28 15:25:38 +0000

    Implementing the functionality to publish to HDX

---
+++ hdx_floodscan.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python
+"""
+HDX Pipeline:
+------------
+
+TODO
+- Add summary about this dataset pipeline
+
+"""
+import logging
+import os
+from copy import copy
+from datetime import datetime, timezone
+import pandas as pd
+from hdx.data.dataset import Dataset
+from slugify import slugify
+import xarray as xr
+import shutil
+
+from src.utils.date_utils import create_date_range, get_start_and_last_date_from_90_days
+
+logger = logging.getLogger(__name__)
+DATE_FORMAT = "%Y-%m-%d"
+
+
+class HDXFloodscan:
+    def __init__(self, configuration, retriever, folder, errors):
+        self.configuration = configuration
+        self.retriever = retriever
+        self.folder = folder
+        self.manual_url = None
+        self.dataset_data = {}
+        self.errors = errors
+        self.created_date = None
+        self.start_date = None
+        self.latest_date = None
+
+    def get_data(self):
+
+        try:
+            url = os.environ["BLOB_URL"]
+            account = os.environ["STORAGE_ACCOUNT"]
+            container = os.environ["CONTAINER"]
+            key = os.environ["KEY"]
+        except Exception:
+            url = self.configuration["url"]
+            account = self.configuration["account"]
+            container = self.configuration["container"]
+            key = self.configuration["key"]
+
+        last90_days_filename = self.configuration["last90_days_filename"]
+        stats_filename = self.configuration["stats_filename"]
+        dataset_name = self.configuration["dataset_names"]["HDX-FLOODSCAN"]
+
+        last90_days_files = self._get_latest_90_days_geotiffs(account, container, key)
+        historical_baseline = self._get_historical_baseline(account, container, key, last90_days_filename)
+        last90_days_file = self._generate_zipped_file(last90_days_files, historical_baseline)
+
+        # Find the minimum and maximum dates
+        self.start_date, self.latest_date = get_start_and_last_date_from_90_days(last90_days_file)
+
+        # Save all geotiffs as one zipped file
+        last90_days_file = shutil.make_archive("baseline_zipped_file", 'zip', "geotiffs")
+        shutil.rmtree("geotiffs")
+
+        stats_file = self.retriever.download_file(
+            url=url,
+            account=account,
+            container="dsci", # TODO change this to parameter before pushing changes to prod
+            key=key,
+            blob=stats_filename)
+
+        data_df_stats = pd.read_excel(stats_file, sheet_name="FloodScan", keep_default_na=False).replace('[“”]', '', regex=True)
+
+        self.dataset_data[dataset_name] = [data_df_stats.apply(lambda x: x.to_dict(), axis=1),
+                                           last90_days_file]
+
+        self.created_date = datetime.fromtimestamp((os.path.getctime(stats_file)), tz=timezone.utc)
+        return [{"name": dataset_name}]
+
+    def generate_dataset_and_showcase(self, dataset_name):
+
+        # Setting metadata and configurations
+        name = self.configuration["dataset_names"]["HDX-FLOODSCAN"]
+        title = self.configuration["title"]
+        update_frequency = self.configuration["update_frequency"]
+        dataset = Dataset({"name": slugify(name), "title": title})
+        rows = self.dataset_data[dataset_name][0]
+        dataset.set_maintainer(self.configuration["maintainer_id"])
+        dataset.set_organization(self.configuration["organization_id"])
+        dataset.set_expected_update_frequency(update_frequency)
+        dataset.set_subnational(False)
+        dataset.add_other_location("world")
+        dataset["notes"] = self.configuration["notes"]
+        filename = "hdx_floodscan_zonal_stats.xlsx"
+        resource_data = {"name": filename,
+                         "description": self.configuration["description_stats_file"]}
+        tags = sorted([t for t in self.configuration["allowed_tags"]])
+        dataset.add_tags(tags)
+
+        # Setting time period
+        start_date = self.start_date
+        ongoing = False
+        if not start_date:
+            logger.error(f"Start date missing for {dataset_name}")
+            return None, None
+        dataset.set_time_period(start_date, self.latest_date, ongoing)
+
+        headers = rows[0].keys()
+        date_headers = [h for h in headers if "date" in h.lower() and type(rows[0][h]) == int]
+        for row in rows:
+            for date_header in date_headers:
+                row_date = row[date_header]
+                if not row_date:
+                    continue
+                if len(str(row_date)) > 9:
+                    row_date = row_date / 1000
+                row_date = datetime.utcfromtimestamp(row_date)
+                row_date = row_date.strftime("%Y-%m-%d")
+                row[date_header] = row_date
+
+        rows
+        dataset.generate_resource_from_rows(
+            self.folder,
+            filename,
+            rows,
+            resource_data,
+            list(rows[0].keys()),
+            encoding='utf-8'
+        )
+
+        second_filename = "aer_floodscan_300s_SFED_90d.zip"
+        resource_data = {"name": second_filename,
+                         "description": self.configuration["description_90days_file"]}
+
+        res = copy(dataset.get_resource(0))
+        dataset.resources.append(res)
+        resource = dataset.get_resource(1)
+        resource.set_format("zipped geotiff")
+        resource['name'] = resource_data['name']
+        resource['description'] = resource_data['description']
+        resource.set_file_to_upload(self.dataset_data[dataset_name][1])
+        dataset.add_update_resource(resource)
+
+        return dataset
+
+    def _get_latest_90_days_geotiffs(self, account, container, key):
+
+        geotiffs = []
+        # TODO add back yesterday = datetime.today() - pd.DateOffset(days=1)
+        yesterday = datetime.strptime("2024-01-01", "%Y-%m-%d")
+        dates = create_date_range(90, yesterday)
+
+        for date in dates:
+            blob = f"floodscan/daily/v5/processed/aer_area_300s_v{date.strftime(DATE_FORMAT)}_v05r01.tif"
+
+            geotiff_file_for_date = self.retriever.download_file(
+                url=blob,
+                account=account,
+                container=container,
+                key=key,
+                blob=blob)
+            ds = xr.open_dataset(geotiff_file_for_date)
+            ds['date'] = date
+            geotiffs.append(ds.sel({"band": 1}, drop=True))
+
+        return geotiffs
+
+    def _get_historical_baseline(self, account, container, key, blob):
+
+        historical_baseline_file = self.retriever.download_file(
+                url=blob,
+                account=account,
+                container=container,
+                key=key,
+                blob=blob)
+        ds_historical_baseline = xr.open_dataset(historical_baseline_file)
+
+        return ds_historical_baseline
+
+    def _generate_zipped_file(self, last90_days_geotiffs, ds_historical_baseline):
+
+        os.makedirs('geotiffs', exist_ok=True)
+
+        out_files = []
+        for ds_current_sfed in last90_days_geotiffs:
+            ds_historical_baseline = ds_historical_baseline.persist()
+
+            dt_temp_str = ds_current_sfed.date.dt.strftime("%Y%m%d")
+            doy_temp = int(ds_current_sfed["date"].dt.dayofyear)
+            ds_current_sfed = ds_current_sfed.drop_vars(["date"])
+            h_sfed_temp = ds_historical_baseline.sel({"dayofyear": doy_temp}, drop=True)
+
+            h_sfed_temp_interp = h_sfed_temp.interp_like(ds_current_sfed, method="nearest")
+            merged_temp = xr.merge([ds_current_sfed, h_sfed_temp_interp])
+            merged_temp= merged_temp.drop_vars(["band_data"])
+            merged_temp = merged_temp.rename({'__xarray_dataarray_variable__' : "SFED_BASELINE"})
+
+            out_file = f"geotiffs/{int(dt_temp_str)}_aer_floodscan_sfed.tif"
+
+            # Save geotiff
+            merged_temp.rio.to_raster(out_file, driver="COG")
+            out_files.append(out_file)
+
+        return out_files
+
+
---
+++ run.py
@@ -0,0 +1,280 @@
+#!/usr/bin/python
+"""
+Top level script. Calls other functions that generate datasets that this script then creates in HDX.
+
+"""
+import logging
+from os.path import expanduser, join, exists
+from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
+from hdx.utilities.path import progress_storing_folder, wheretostart_tempdir_batch
+from hdx.utilities.retriever import Retrieve
+from hdx.data.hdxobject import HDXError
+from datetime import datetime
+import hmac
+import hashlib
+import base64
+from hdx_floodscan import HDXFloodscan
+
+"""Facade to simplify project setup that calls project main function with kwargs"""
+
+import sys
+from inspect import getdoc
+from typing import Any, Callable, Optional  # noqa: F401
+import defopt
+from makefun import with_signature
+from hdx.api import __version__
+from hdx.api.configuration import Configuration
+from hdx.utilities.useragent import UserAgent
+
+logger = logging.getLogger(__name__)
+
+lookup = "hdx-floodscan"
+updated_by_script = "HDX Scraper: HDX FloodScan"
+
+
+class AzureBlobDownload(Download):
+
+    def download_file(
+            self,
+            url: str,
+            account: str,
+            container: str,
+            key: str,
+            blob: None,
+            **kwargs: Any,
+    ) -> str:
+        """Download file from blob storage and store in provided folder or temporary
+        folder if no folder supplied.
+
+        Args:
+            url (str): URL for the exact blob location
+            account (str): Storage account to access the blob
+            container (str): Container to download from
+            key (str): Key to access the blob
+            blob (str): Name of the blob to be downloaded. If empty, then it is assumed to download the whole container.
+            **kwargs: See below
+            folder (str): Folder to download it to. Defaults to temporary folder.
+            filename (str): Filename to use for downloaded file. Defaults to deriving from url.
+            path (str): Full path to use for downloaded file instead of folder and filename.
+            overwrite (bool): Whether to overwrite existing file. Defaults to False.
+            keep (bool): Whether to keep already downloaded file. Defaults to False.
+            post (bool): Whether to use POST instead of GET. Defaults to False.
+            parameters (Dict): Parameters to pass. Defaults to None.
+            timeout (float): Timeout for connecting to URL. Defaults to None (no timeout).
+            headers (Dict): Headers to pass. Defaults to None.
+            encoding (str): Encoding to use for text response. Defaults to None (best guess).
+
+        Returns:
+            str: Path of downloaded file
+        """
+        folder = kwargs.get("folder")
+        filename = kwargs.get("filename")
+        path = kwargs.get("path")
+        overwrite = kwargs.get("overwrite", False)
+        keep = kwargs.get("keep", False)
+
+        request_time = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        api_version = '2018-03-28'
+        parameters = {
+            'verb': 'GET',
+            'Content-Encoding': '',
+            'Content-Language': '',
+            'Content-Length': '',
+            'Content-MD5': '',
+            'Content-Type': '',
+            'Date': '',
+            'If-Modified-Since': '',
+            'If-Match': '',
+            'If-None-Match': '',
+            'If-Unmodified-Since': '',
+            'Range': '',
+            'CanonicalizedHeaders': 'x-ms-date:' + request_time + '\nx-ms-version:' + api_version + '\n',
+            'CanonicalizedResource': '/' + account + '/' + container + '/' + blob
+        }
+
+        signature = (parameters['verb'] + '\n'
+                          + parameters['Content-Encoding'] + '\n'
+                          + parameters['Content-Language'] + '\n'
+                          + parameters['Content-Length'] + '\n'
+                          + parameters['Content-MD5'] + '\n'
+                          + parameters['Content-Type'] + '\n'
+                          + parameters['Date'] + '\n'
+                          + parameters['If-Modified-Since'] + '\n'
+                          + parameters['If-Match'] + '\n'
+                          + parameters['If-None-Match'] + '\n'
+                          + parameters['If-Unmodified-Since'] + '\n'
+                          + parameters['Range'] + '\n'
+                          + parameters['CanonicalizedHeaders']
+                          + parameters['CanonicalizedResource'])
+
+        signed_string = base64.b64encode(hmac.new(base64.b64decode(key), msg=signature.encode('utf-8'),
+                                                  digestmod=hashlib.sha256).digest()).decode()
+
+        headers = {
+            'x-ms-date': request_time,
+            'x-ms-version': api_version,
+            'Authorization': ('SharedKey ' + account + ':' + signed_string)
+        }
+
+        url = ('https://' + account + '.blob.core.windows.net/' + container + '/' + blob)
+
+        if keep and exists(url):
+            print(f"The blob URL exists: {url}")
+            return path
+        self.setup(
+            url=url,
+            stream=True,
+            post=kwargs.get("post", False),
+            parameters=kwargs.get("parameters"),
+            timeout=kwargs.get("timeout"),
+            headers=headers,
+            encoding=kwargs.get("encoding"),
+        )
+        return self.stream_path(
+            path, f"Download of {url} failed in retrieval of stream!"
+        )
+
+ # TODO Improve logic around facade
+def facade(projectmainfn: Callable[[Any], None], **kwargs: Any):
+    """Facade to simplify project setup that calls project main function. It infers
+    command line arguments from the passed in function using defopt. The function passed
+    in should have type hints and a docstring from which to infer the command line
+    arguments. Any **kwargs given will be merged with command line arguments, with the
+    command line arguments taking precedence.
+
+    Args:
+        projectmainfn ((Any) -> None): main function of project
+        **kwargs: Configuration parameters to pass to HDX Configuration & other parameters to pass to main function
+
+    Returns:
+        None
+    """
+
+    #
+    # Setting up configuration
+    #
+
+    parsed_main_doc = defopt._parse_docstring(getdoc(projectmainfn))
+    main_doc = [f"{parsed_main_doc.first_line}\n\nArgs:"]
+    no_main_params = len(parsed_main_doc.params)
+    for param_name, param_info in parsed_main_doc.params.items():
+        main_doc.append(
+            f"\n    {param_name} ({param_info.type}): {param_info.text}"
+        )
+    create_config_doc = getdoc(Configuration.create)
+    kwargs_index = create_config_doc.index("**kwargs")
+    kwargs_index = create_config_doc.index("\n", kwargs_index)
+    args_doc = create_config_doc[kwargs_index:]
+    main_doc.append(args_doc)
+    main_doc = "".join(main_doc)
+
+    main_sig = defopt.signature(projectmainfn)
+    param_names = []
+    for param in main_sig.parameters.values():
+        param_names.append(str(param))
+
+    parsed_main_doc = defopt._parse_docstring(main_doc)
+    main_doc = [f"{parsed_main_doc.first_line}\n\nArgs:"]
+    count = 0
+    for param_name, param_info in parsed_main_doc.params.items():
+        param_type = param_info.type
+        if param_type == "dict":
+            continue
+        if count < no_main_params:
+            count += 1
+        else:
+            if param_type == "str":
+                param_type = "Optional[str]"
+                default = None
+            elif param_type == "bool":
+                default = False
+            else:
+                raise ValueError(
+                    "Configuration.create has new parameter with unknown type!"
+                )
+            param_names.append(f"{param_name}: {param_type} = {default}")
+        main_doc.append(
+            f"\n    {param_name} ({param_type}): {param_info.text}"
+        )
+    main_doc = "".join(main_doc)
+
+    projectmainname = projectmainfn.__name__
+    main_sig = f"{projectmainname}({','.join(param_names)})"
+
+    argv = sys.argv[1:]
+    for key in kwargs:
+        name = f"--{key.replace('_', '-')}"
+        if name not in argv:
+            argv.append(name)
+            argv.append(kwargs[key])
+
+    @with_signature(main_sig)
+    def gen_func(*args, **kwargs):
+        """docstring"""
+        site_url = Configuration._create(*args, **kwargs)
+        logger.info("--------------------------------------------------")
+        logger.info(f"> Using HDX Python API Library {__version__}")
+        logger.info(f"> HDX Site: {site_url}")
+
+    gen_func.__doc__ = main_doc
+
+    configuration_create = defopt.bind(gen_func, argv=argv, cli_options="all")
+    main_func, _ = defopt.bind_known(
+        projectmainfn, argv=argv, cli_options="all"
+    )
+
+    configuration_create()
+    UserAgent.user_agent = Configuration.read().user_agent
+    main_func()
+
+
+def main(save: bool = False, use_saved: bool = False) -> None:
+    """Generate datasets and create them in HDX"""
+    with ErrorsOnExit() as errors:
+        with wheretostart_tempdir_batch(lookup) as info:
+            folder = info["folder"]
+            with AzureBlobDownload() as downloader:
+                retriever = Retrieve(
+                    downloader, folder, "saved_data", folder, save, use_saved
+                )
+                folder = info["folder"]
+                batch = info["batch"]
+                configuration = Configuration.read()
+                floodscan = HDXFloodscan(configuration, retriever, folder, errors)
+                dataset_names = floodscan.get_data()
+                logger.info(f"Number of datasets to upload: {len(dataset_names)}")
+
+                for _, nextdict in progress_storing_folder(info, dataset_names, "name"):
+                    dataset_name = nextdict["name"]
+                    dataset = floodscan.generate_dataset_and_showcase(dataset_name=dataset_name)
+                    if dataset:
+                        dataset.update_from_yaml()
+                        dataset["notes"] = dataset["notes"].replace(
+                            "\n", "  \n"
+                        )  # ensure markdown has line breaks
+                        try:
+                            dataset.create_in_hdx(
+                                remove_additional_resources=True,
+                                hxl_update=False,
+                                updated_by_script=updated_by_script,
+                                batch=batch,
+                                ignore_fields=["resource:description", "extras"],
+                            )
+                        except HDXError as err:
+                            errors.add(f"Could not upload {dataset_name}: {err}")
+                            continue
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    facade(
+        main,
+        user_agent_config_yaml="/Workspace/Shared/init-scripts/.useragents.yaml",
+        hdx_config_yaml="/Workspace/Shared/init-scripts/.hdx_configuration.yaml",
+        user_agent_lookup=lookup,
+        project_config_yaml=join("config", "project_configuration.yaml"),
+
+    )
